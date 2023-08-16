f3760db143989abbed122dc1dfc306b654826e01
Sebastian <seba.kowal2001@gmail.com>
2023-08-16 12:46:02 +0200

    Linkowanie 3.0

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,12 @@ const App = () => {
     <div className="app">
       <p className=""> Numer projektu Tytuł projekty </p>
       <div className="gallery">
-        <img src="./polska.jpg" alt="Flag 1" className='center-image'/>
-        <img src="./hiszpania.jpg" alt="Flag 2" className='center-image'/>
+        <Link href="./src" className="link">
+          <img src="./polska.jpg" alt="Flag 1" className="center-image" />
+        </Link>
+        <Link href="./src" className="link">
+          <img src="./hiszpania.jpg" alt="Flag 2" className="center-image" />
+        </Link>
       </div>
       <img src="./unia.jpg" alt="Flag 3" className="bottom-image" />
     </div>
